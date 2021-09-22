4a8ba7434fced730b560e2cbdaa8fdabaf3a9098
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-23 08:06:46 +0900

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -4,14 +4,14 @@ function h($str) {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
-$timestamp = time() ;
 $symbol = (string)filter_input(INPUT_POST, 'symbol'); // $_POST['symbol']
 $color = (string)filter_input(INPUT_POST, 'color'); // $_POST['color']
+$timestamp = time() ;
 
 $fp = fopen('symbol_color.csv', 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
-    fputcsv($fp, [$timestamp, $symbol, $color]);
+    fputcsv($fp, [$symbol, $color, $timestamp,]);
     rewind($fp);
 }
 flock($fp, LOCK_SH);

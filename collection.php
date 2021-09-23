4ae8982e6b1c7a949178157c30919212deed810f
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-23 09:04:34 +0900

    sign

---
+++ collection.php
@@ -0,0 +1,54 @@
+<?php
+
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+$symbol = (string)filter_input(INPUT_POST, 'symbol'); // $_POST['symbol']
+$color = (string)filter_input(INPUT_POST, 'color'); // $_POST['color']
+$timestamp = time() ;
+
+$forwardedFor = $_SERVER["HTTP_X_FORWARDED_FOR"];
+$ips = explode(",", $forwardedFor);
+$ip = $ips[0];
+
+$fp = fopen('symbol_color.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$symbol, $color, $timestamp, $ip]);
+    rewind($fp);
+}
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="symbol_color.css" />
+<title>自分の気持ちを知る・表す</title>
+<style type="text/css">
+</style>
+</head>
+<body>
+  
+<ul id="symbol_color">
+<?php if (!empty($rows)): ?>
+<?php foreach ($rows as $row): ?>
+<li class="bg_color" style="background:#<?=h($row[1])?>;">
+<span class="bg_symbol"><?=h($row[0])?></span>
+</li>
+<?php endforeach; ?>
+<?php else: ?>
+<li class="bg_color" style="background:#fff;">
+<span class="bg_symbol">✔</span>
+</li>
+<?php endif; ?>
+</ul>
+</body>

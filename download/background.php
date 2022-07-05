8d2cf47570bdf6cc684fb9fc1a2fd29ae1fd831e
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 06:24:45 +0900

    Update background.php

--- download/background.php
+++ download/background.php
@@ -7,16 +7,9 @@ function h($str) {
 }
 
 $today = date("d");
-$symbol = (string)filter_input(INPUT_POST, 'symbol'); // $_POST['symbol']
-$color = (string)filter_input(INPUT_POST, 'color'); // $_POST['color']
-$timestamp = date("g:i:s A T");
-$filename =  $today . ".csv"; 
+$source_file =  $today . ".csv";
 
-$forwardedFor = $_SERVER["REMOTE_ADDR"];
-$ips = explode(",", $forwardedFor);
-$ip = $ips[0];
-
-$fp = fopen($filename, 'a+b');
+$fp = fopen($source_file, 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
     fputcsv($fp, [$symbol, $color, $timestamp, $ip,]);

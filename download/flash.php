587a78e938f4da1bd9053b6ad40dbff91f742f95
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 06:31:23 +0900

    Update flash.php

--- download/flash.php
+++ download/flash.php
@@ -7,14 +7,7 @@ function h($str) {
 }
 
 $today = date("d");
-$symbol = (string)filter_input(INPUT_POST, 'symbol'); // $_POST['symbol']
-$color = (string)filter_input(INPUT_POST, 'color'); // $_POST['color']
-$timestamp = date("g:i:s A T");
-$filename =  $today . ".csv"; 
-
-$forwardedFor = $_SERVER["REMOTE_ADDR"];
-$ips = explode(",", $forwardedFor);
-$ip = $ips[0];
+$filename =  $today . ".csv";
 
 $fp = fopen($filename, 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {

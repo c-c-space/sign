f7ff1005c5f35e814878ddb679a051eafeab9521
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 05:05:03 +0900

    Update log.php

--- download/log.php
+++ download/log.php
@@ -154,8 +154,10 @@ fclose($fp);
                 <?php else: ?>
                 <li>
                     <p>
-                        <i><?php echo $_SERVER['REMOTE_ADDR']; ?></i>
+                        <u style="background:#000;"><span>IP</span></u>
+                        <b style="color:#000>; filter: invert();"><?php echo $_SERVER['REMOTE_ADDR']; ?></b>
                     </p>
+                    <p id="showTime"></p>
                 </li>
                 <?php endif; ?>
                 <li>
@@ -173,6 +175,24 @@ fclose($fp);
                 </li>
             </ul>
         </div>
+<script type="text/javascript">
+function set2(num) {
+  let ret;
+  if (num < 10) { ret = "0" + num; }
+  else { ret = num; }
+  return ret;
+}
+function showClock() {
+  const nowTime = new Date();
+  const nowHour = set2(nowTime.getHours());
+  const nowMin = set2(nowTime.getMinutes());
+  const nowSec = set2(nowTime.getSeconds());
+  const msg = "" + nowHour + ":" + nowMin + ":" + nowSec + "";
+  document.getElementById("showTime").innerHTML = msg;
+}
+setInterval('showClock()', 1000);
+</script>
+
     </body>
 
     </html>

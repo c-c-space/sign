2ac21c97d2c46b0e196b80825cd13e107a98cc7a
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-07 05:44:04 +0900

    Update log.php

--- 2021/04/log.php
+++ 2021/04/log.php
@@ -276,7 +276,7 @@ fclose($fp);
             <b id="ed">𝕹𝖊𝖜 𝕷𝖎𝖋𝖊 𝕮𝖔𝖑𝖑𝖊𝖈𝖙𝖎𝖔𝖓</b>
             <p id="today">
                 <sup id="no" style="text-transform: uppercase;">令和三年四月</sup>
-                <sup id="post" style="text-transform: uppercase;">自分の気持ちを知る・表す</sup>
+                <sup id="post" style="text-transform: uppercase;">の 色 と 記号</sup>
             </p>
             <p id="credit"><img src="/sign/qr.png" width="100%"></p>
         </div>

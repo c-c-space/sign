738b1ad69ecbc83bcdbce74a24cc9667328b0361
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-07 07:24:44 +0900

    style

--- beta/index.php
+++ beta/index.php
@@ -23,32 +23,88 @@ fclose($fp);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>自分の気持ちを知る・表す</title>
     <style type="text/css">
-    @media screen and (max-width: 750px) {
-      .library li {
-        height: 90vh;
-      }
-      .library::-webkit-scrollbar {
-        height: 0;
-      }
-    }
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        
+        .library {
+            padding: 0;
+            margin: 0;
+            height: 100vh;
+            white-space: nowrap;
+            overflow: auto;
+            -webkit-overflow-scrolling: touch;
+        }
+        
+        .library>* {
+            display: inline-block;
+        }
+        
+        .library li {
+            width: 45rem;
+            max-width: 90vw;
+            overflow: hidden;
+        }
+        
+        .library li iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+        
+        .library::-webkit-scrollbar {
+            width: 0;
+            height: 5vh;
+            background-size: 400% 400%;
+            animation: gradientBG 5s ease infinite;
+            background: linear-gradient(-90deg, #fffafa, #483d8b, #f0ffff, #f0ffff, #d2b48c, #0000cd, #ff8c00, #008080, #808080, #fffafa, #fff000, #fffafa, #b0c4de, #808080, #b0c4de, #d2b48c, #808080, #ffdab9, #b0c4de, #808080, #ba55d3, #ff0000, #90ee90, #fff);
+        }
+        
+        .library::-webkit-scrollbar-thumb {
+            background: #fff;
+        }
+        
+        @media screen and (max-width: 750px) {
+            .library {
+                height: 95vh;
+            }
+            .library::-webkit-scrollbar {
+                height: 2.5vh;
+            }
+        }
+        
+        @keyframes gradientBG {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+        }
     </style>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
+</head>
+
+<body>
     <ul class="mousedragscrollable library">
-      <?php if (!empty($rows)): ?>
+        <?php if (!empty($rows)): ?>
         <?php foreach ($rows as $row): ?>
-          <li><iframe src="<?=h($row[1])?>"></iframe></li>
-          <?php endforeach; ?>
+        <li><iframe src="<?=h($row[1])?>"></iframe></li>
+        <?php endforeach; ?>
         <?php else: ?>
-      <?php endif; ?>
+        <?php endif; ?>
     </ul>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://creative-community.space/coding/js/mousedragscrollable/scrollable.js"></script>
-  </body>
+</body>
+
 </html>

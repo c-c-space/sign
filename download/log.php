793d252347c60d45d7aad0083e8540a1d459483f
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 16:12:21 +0900

    Update log.php

--- download/log.php
+++ download/log.php
@@ -171,7 +171,7 @@ fclose($fp);
                         <u style="background:#000;"><span style="color:#fff;">?</span></u>
                         <b style="color:#000;">Under Construction</b>
                     </p>
-                    <p id="showTime"></p>
+                    <p>IP <i><?php echo $_SERVER['REMOTE_ADDR']; ?></i></p>
                 </li>
                 <?php endif; ?>
                 <li>

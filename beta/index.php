54b3539d965ee361cfd5611c6a0a37be7bac66c2
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-07 07:43:34 +0900

    sign

--- beta/index.php
+++ beta/index.php
@@ -1,16 +1,21 @@
 <?php
 
+date_default_timezone_set('Asia/Tokyo');
+
 function h($str) {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
-$date = (string)filter_input(INPUT_POST, 'date'); // $_POST['date']
-$link = (string)filter_input(INPUT_POST, 'link'); // $_POST['link']
+$w = date("w");
+$week_name = array("日", "月", "火", "水", "木", "金", "土");
 
-$fp = fopen('index.csv', 'a+b');
+$today = date("Ymd");
+$source_file = "symbol_color.csv";
+
+$fp = fopen($source_file, 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
-    fputcsv($fp, [$symbol, $color]);
+    fputcsv($fp, [$symbol, $color, $timestamp, $ip,]);
     rewind($fp);
 }
 flock($fp, LOCK_SH);
@@ -21,93 +26,199 @@ flock($fp, LOCK_UN);
 fclose($fp);
 
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>自分の気持ちを知る・表す</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>令和三年三月 | 自分の気持ちを知る・表す</title>
     <style type="text/css">
-        body {
-            margin: 0;
-            padding: 0;
-            overflow-y: hidden;
-        }
-        
-        .library {
-            padding: 0;
-            margin: 0;
-            height: 100vh;
-            white-space: nowrap;
-            overflow-x: auto;
-            overflow-y: hidden;
-            -webkit-overflow-scrolling: touch;
-        }
-        
-        .library>* {
+        .nlc {
+            font-family: 'Times New Roman', serif;
+            font-weight: 500;
+            line-height: 200%;
+            font-stretch: condensed;
+            font-variant: common-ligatures tabular-nums;
             display: inline-block;
+            transform: scale(1, 1.1);
+            word-spacing: -.25ch;
         }
         
-        .library li {
-            width: 45rem;
-            height: 99.5%;
-            max-width: 90vw;
-            overflow: hidden;
+        #update {
+            position: fixed;
+            top: 2.5vw;
+            right: 2.5vw;
+            z-index: 50;
+            color: #000;
+            border-radius: 50%;
+            text-decoration: none;
+            transition: .5s all;
+            width: 3vw;
+            height: 3vw;
         }
         
-        .library li iframe {
+        #update:hover {
+            cursor: pointer;
+            color: #fff;
+            transition: 1s all;
+        }
+        
+        #update b {
+            position: absolute;
+            padding: 0;
+            margin: 0;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -webkit-transform: translate(-50%, -50%);
+            font-weight: 500;
+            letter-spacing: .1vw;
+            font-family: "SimSong", "MS Mincho", serif;
+            font-size: 2.5vw;
+        }
+        
+        #menu {
+            position: fixed;
+            z-index: 100;
+            bottom: 0;
+            left: 0;
+            width: 95%;
+            padding: 0.25rem 2.5%;
+            font-size: 1rem;
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+        }
+        
+        #menu .tab {
+            color: #000;
+            text-decoration: none;
+            transition: all 500ms ease;
+        }
+        
+        #menu .tab:hover,
+        #menu .check b {
+            cursor: pointer;
+            color: #fff;
+            transition: all 500ms ease;
+        }
+        
+        #menu .check {
+            float: left;
+            display: inline-block;
+            width: 2.5rem;
+            margin-right: 0rem;
+            text-align: center;
+        }
+        
+        #menu .check:before {
+            content: '[';
+            opacity: 1;
+        }
+        
+        #menu .check:after {
+            content: ']';
+            opacity: 1;
+        }
+        
+        .check b {
+            opacity: 0;
+            transition: all 1000ms ease;
+        }
+        
+        .tab:hover+.check b {
+            opacity: 1;
+            transition: all 1000ms ease;
+        }
+        
+        #background,
+        #flash,
+        #sign {
+            position: fixed;
+            width: 100vw;
+            height: 100vh;
+            top: 0;
+            left: 0;
+        }
+        
+        #background {
+            z-index: -1;
+        }
+        
+        #flash iframe {
             width: 100%;
             height: 100%;
             border: none;
         }
         
-        .library::-webkit-scrollbar {
-            width: 0;
-            height: 4.5vh;
-            background-size: 400% 400%;
-            animation: gradientBG 5s ease infinite;
-            background: linear-gradient(-90deg, #fffafa, #483d8b, #f0ffff, #f0ffff, #d2b48c, #0000cd, #ff8c00, #008080, #808080, #fffafa, #fff000, #fffafa, #b0c4de, #808080, #b0c4de, #d2b48c, #808080, #ffdab9, #b0c4de, #808080, #ba55d3, #ff0000, #90ee90, #fff);
-        }
-        
-        .library::-webkit-scrollbar-thumb {
-            background: #fff;
-        }
-        
         @media screen and (max-width: 550px) {
-            .library {
-                height: 85vh;
+            #update {
+                z-index: 50;
+                width: 2rem;
+                height: 2rem;
             }
-            .library::-webkit-scrollbar {
-                height: 0;
+            #update b {
+                letter-spacing: .1rem;
+                font-size: 1.5rem;
             }
         }
         
-        @keyframes gradientBG {
-            0% {
-                background-position: 0% 50%;
-            }
-            50% {
-                background-position: 100% 50%;
-            }
-            100% {
-                background-position: 0% 50%;
+        @media print {
+            #menu,
+            #update,
+            #index {
+                display: none;
             }
         }
     </style>
 </head>
 
 <body>
-    <ul class="mousedragscrollable library">
-        <?php if (!empty($rows)): ?>
-        <?php foreach ($rows as $row): ?>
-        <li><iframe src="<?=h($row[1])?>"></iframe></li>
-        <?php endforeach; ?>
-        <?php else: ?>
-        <?php endif; ?>
-    </ul>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://creative-community.space/coding/js/mousedragscrollable/scrollable.js"></script>
+    <a id="update" href="print.pdf" target="_parent">
+        <b>⎷</b>
+    </a>
+
+    <div id="menu" class="nlc">
+        <div>
+            <a class="tab" href="#sign">March 2021</a>
+            <span class="check"><b>✔</b></span>
+        </div>
+        <div>
+            <a class="tab" href="#flash">View The Collection</a>
+            <span class="check"><b>✔</b></span>
+        </div>
+    </div>
+
+    <div id="background"></div>
+    <div id="sign" class="change"></div>
+    <div id="flash" class="change"><iframe src="flash.php"></iframe></div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $("#background").load("background.php");
+            $("#sign").load("log.php");
+        })
+
+        $(function() {
+            $('.change').hide();
+
+            $('.tab').on('click', function() {
+                $('.change').not($($(this).attr('href'))).hide();
+                $($(this).attr('href')).fadeToggle(1000);
+            });
+        });
+
+        $('a[href^="#"]').click(function() {
+            var href = $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            return false;
+        });
+    </script>
 </body>
 
 </html>

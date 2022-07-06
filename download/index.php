26dc43337f3f96023d9b26fa0ee92394facb0f67
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-06 14:55:54 +0900

    sign

--- download/index.php
+++ download/index.php
@@ -200,22 +200,6 @@ fclose($fp);
             var target = $(href == "#" || href == "" ? 'html' : href);
             return false;
         });
-
-function set2(num) {
-  let ret;
-  if (num < 10) { ret = "0" + num; }
-  else { ret = num; }
-  return ret;
-}
-function showClock() {
-  const nowTime = new Date();
-  const nowHour = set2(nowTime.getHours());
-  const nowMin = set2(nowTime.getMinutes());
-  const nowSec = set2(nowTime.getSeconds());
-  const msg = "" + nowHour + ":" + nowMin + ":" + nowSec + "";
-  document.getElementById("showTime").innerHTML = msg;
-}
-setInterval('showClock()', 1000);
     </script>
 </body>
 

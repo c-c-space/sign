52b4fb21d7362581d261801d9a036288a42f9f21
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-08 20:39:04 +0900

    Update submit.js

--- submit/submit.js
+++ submit/submit.js
@@ -4,11 +4,11 @@ $(window).click(function() {
 });
 
 var volume = new Tone.Volume(5);
-var click = new Tone.FMSynth(5, Tone.Synth).chain(volume, Tone.Master);
-var notes = Tone.Frequency("G4").harmonize([1, 3, 6, 8, 10, ]);
-var noteIndex = 0;
+var click = new Tone.Synth(5, Tone.Synth).chain(volume, Tone.Master);
+var number = Tone.Frequency("F3").harmonize([1, 3, 6, 8, 10, ]);
+var numberIndex = 0;
 
 $("#click .click").click(function(e) {
-    var randNote = Math.floor(Math.random() * notes.length);
-    click.triggerAttackRelease(notes[randNote], "10n");
+    var numberNote = Math.floor(Math.random() * number.length);
+    click.triggerAttackRelease(number[numberNote], "10n");
 });

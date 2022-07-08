34e52a7e3e8f7c35429e3838641b16f8b2f8cd3f
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-08 20:32:32 +0900

    Update submit.js

--- submit/submit.js
+++ submit/submit.js
@@ -5,15 +5,7 @@ $(window).click(function() {
 
 var volume = new Tone.Volume(5);
 var click = new Tone.FMSynth(5, Tone.Synth).chain(volume, Tone.Master);
-var notes = Tone.Frequency("G4").harmonize([
-    1, 3, 6, 8, 10,
-    3, 6, 8, 10, 13,
-    6, 8, 10, 13, 15,
-    8, 10, 13, 15, 18,
-    10, 13, 15, 18, 20,
-    13, 15, 18, 20, 22,
-    15, 18, 20, 22, 25
-]);
+var notes = Tone.Frequency("G4").harmonize([1, 3, 6, 8, 10, ]);
 var noteIndex = 0;
 
 $("#click .click").click(function(e) {

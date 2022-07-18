9b00451f6f3fa6a38d1d633322b148b997197497
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-18 21:36:59 +0900

    Update visitorsonly.php

--- bnaaltermuseum/visitorsonly.php
+++ bnaaltermuseum/visitorsonly.php
@@ -239,7 +239,6 @@ fclose($fp);
 
     $(function() {
         $('.change').hide();
-
         $('.tab').on('click', function() {
             $('.change').not($($(this).attr('href'))).hide();
             $($(this).attr('href')).fadeToggle(1000);

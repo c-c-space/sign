c9e7de611fd13244849bf31a18d74aa2525f676a
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-23 13:53:58 +0900

    Update mobile.php

--- bnaaltermuseum/mobile.php
+++ bnaaltermuseum/mobile.php
@@ -94,7 +94,7 @@ fclose($fp);
         #btn {
             position: fixed;
             top: 2.5vw;
-            left: 5vw;
+            left: 4.5vw;
             z-index: 100;
             color: #000;
             border: solid 0.1vw #000;

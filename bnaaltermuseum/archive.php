a3fdb65bf9324390cbf70aefbd0546e172acde3b
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-20 00:28:12 +0900

    sign

--- bnaaltermuseum/archive.php
+++ bnaaltermuseum/archive.php
@@ -298,8 +298,8 @@ while ($row = fgetcsv($fp)) {
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript">
     $(function() {
-        $("#flash").load("flash.php");
-        $("#log").load("log.php");
+        $("#flash").load("ramdom.php");
+        $("#log").load("all.php");
     })
 
     $(function() {

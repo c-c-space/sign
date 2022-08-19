1566e5c88ece582b7d87ce31afe8cbb287b8b6c4
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-19 23:23:35 +0900

    Update archive.php

--- bnaaltermuseum/archive.php
+++ bnaaltermuseum/archive.php
@@ -251,13 +251,13 @@ while ($row = fgetcsv($fp)) {
     <form action="#" method="POST">
         <select name="today">
             <option value="">Choose The Date</option>
-            <option value="0723">2022 年 7 月 23 日 (土)</option>
-            <option value="0724">2022 年 7 月 24 日 (日)</option>
-            <option value="0725">2022 年 7 月 25 日 (月)</option>
-            <option value="0726">2022 年 7 月 26 日 (火)</option>
-            <option value="0727">2022 年 7 月 27 日 (水)</option>
-            <option value="0728">2022 年 7 月 28 日 (木)</option>
-            <option value="0729">2022 年 7 月 29 日 (金)</option>
+            <option value="20220723">2022 年 7 月 23 日 (土)</option>
+            <option value="20220724">2022 年 7 月 24 日 (日)</option>
+            <option value="20220725">2022 年 7 月 25 日 (月)</option>
+            <option value="20220726">2022 年 7 月 26 日 (火)</option>
+            <option value="20220727">2022 年 7 月 27 日 (水)</option>
+            <option value="20220728">2022 年 7 月 28 日 (木)</option>
+            <option value="20220729">2022 年 7 月 29 日 (金)</option>
         </select>
         <input type="submit" name="submit" value="気持ちを表す色と記号"/>
     </form>

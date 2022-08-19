c16cda02c3d1f10299d75d43ff48a114178fdbff
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-19 22:58:49 +0900

    Update archive.php

--- bnaaltermuseum/archive.php
+++ bnaaltermuseum/archive.php
@@ -268,17 +268,19 @@ fclose($fp);
                 echo $today;
             }
             ?>
-            <select name="today">
-                <option value="">Choose The Date</option>
-                <option value="0723">2022 年 7 月 23 日 (土)</option>
-                <option value="0724">2022 年 7 月 24 日 (日)</option>
-                <option value="0725">2022 年 7 月 25 日 (月)</option>
-                <option value="0726">2022 年 7 月 26 日 (火)</option>
-                <option value="0727">2022 年 7 月 27 日 (水)</option>
-                <option value="0728">2022 年 7 月 28 日 (木)</option>
-                <option value="0729">2022 年 7 月 29 日 (金)</option>
-            </select>
-            <input type="submit"name="submit"value="気持ちを表す色と記号"/></sup>
+            <form action="" method="POST">
+                <select name="today">
+                    <option value="">Choose The Date</option>
+                    <option value="0723">2022 年 7 月 23 日 (土)</option>
+                    <option value="0724">2022 年 7 月 24 日 (日)</option>
+                    <option value="0725">2022 年 7 月 25 日 (月)</option>
+                    <option value="0726">2022 年 7 月 26 日 (火)</option>
+                    <option value="0727">2022 年 7 月 27 日 (水)</option>
+                    <option value="0728">2022 年 7 月 28 日 (木)</option>
+                    <option value="0729">2022 年 7 月 29 日 (金)</option>
+                </select>
+                <input type="submit" name="submit" value="気持ちを表す色と記号"/></sup>
+            </form>
         </p>
 
         <div id="credit">

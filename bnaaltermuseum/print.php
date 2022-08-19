e496ab4fffa2cb36b8a47a1aea436ceeb98ccce7
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-20 00:02:34 +0900

    Update print.php

--- bnaaltermuseum/print.php
+++ bnaaltermuseum/print.php
@@ -225,6 +225,12 @@ while ($row = fgetcsv($fp)) {
             background-size: 500% 500%;
             animation: gradient 50s ease infinite;
         }
+
+        #date {
+            position:fixed;
+            bottom:0;
+            width:100%;
+        }
         
         #date select {
             font-size: 1.25rem;
@@ -338,41 +344,41 @@ while ($row = fgetcsv($fp)) {
 
 
     <form id="date" action="" method="POST">
-                <select name="today">
-                    <option value="">令和四年版　夏の自由研究</option>
-                    <option value="20220723">2022 年 7 月 23 日 (土)</option>
-                    <option value="20220724">2022 年 7 月 24 日 (日)</option>
-                    <option value="20220725">2022 年 7 月 25 日 (月)</option>
-                    <option value="20220726">2022 年 7 月 26 日 (火)</option>
-                    <option value="20220727">2022 年 7 月 27 日 (水)</option>
-                    <option value="20220728">2022 年 7 月 28 日 (木)</option>
-                    <option value="20220729">2022 年 7 月 29 日 (金)</option>
-                    <option value="20220730">2022 年 7 月 30 日 (土)</option>
-                    <option value="20220731">2022 年 7 月 31 日 (日)</option>
-                    <option value="20220801">2022 年 8 月 1 日 (月)</option>
-                    <option value="20220802">2022 年 8 月 2 日 (火)</option>
-                    <option value="20220803">2022 年 8 月 3 日 (水)</option>
-                    <option value="20220804">2022 年 8 月 4 日 (木)</option>
-                    <option value="20220805">2022 年 8 月 5 日 (金)</option>
-                    <option value="20220806">2022 年 8 月 6 日 (土)</option>
-                    <option value="20220807">2022 年 8 月 7 日 (日)</option>
-                    <option value="20220808">2022 年 8 月 8 日 (月)</option>
-                    <option value="20220809">2022 年 8 月 9 日 (火)</option>
-                    <option value="20220810">2022 年 8 月 10 日 (水)</option>
-                    <option value="20220811">2022 年 8 月 11 日 (木)</option>
-                    <option value="20220812">2022 年 8 月 12 日 (金)</option>
-                    <option value="20220813">2022 年 8 月 13 日 (土)</option>
-                    <option value="20220814">2022 年 8 月 14 日 (日)</option>
-                    <option value="20220815">2022 年 8 月 15 日 (月)</option>
-                    <option value="20220816">2022 年 8 月 16 日 (火)</option>
-                    <option value="20220817">2022 年 8 月 17 日 (水)</option>
-                    <option value="20220818">2022 年 8 月 18 日 (木)</option>
-                    <option value="20220819">2022 年 8 月 19 日 (金)</option>
-                    <option value="20220820">2022 年 8 月 20 日 (土)</option>
-                    <option value="20220821">2022 年 8 月 21 日 (日)</option>
-                </select>
-                <input type="submit" name="submit" value="決定"/>
-            </form>
+        <select name="today">
+            <option value="">令和四年版　夏の自由研究</option>
+            <option value="20220723">2022 年 7 月 23 日 (土)</option>
+            <option value="20220724">2022 年 7 月 24 日 (日)</option>
+            <option value="20220725">2022 年 7 月 25 日 (月)</option>
+            <option value="20220726">2022 年 7 月 26 日 (火)</option>
+            <option value="20220727">2022 年 7 月 27 日 (水)</option>
+            <option value="20220728">2022 年 7 月 28 日 (木)</option>
+            <option value="20220729">2022 年 7 月 29 日 (金)</option>
+            <option value="20220730">2022 年 7 月 30 日 (土)</option>
+            <option value="20220731">2022 年 7 月 31 日 (日)</option>
+            <option value="20220801">2022 年 8 月 1 日 (月)</option>
+            <option value="20220802">2022 年 8 月 2 日 (火)</option>
+            <option value="20220803">2022 年 8 月 3 日 (水)</option>
+            <option value="20220804">2022 年 8 月 4 日 (木)</option>
+            <option value="20220805">2022 年 8 月 5 日 (金)</option>
+            <option value="20220806">2022 年 8 月 6 日 (土)</option>
+            <option value="20220807">2022 年 8 月 7 日 (日)</option>
+            <option value="20220808">2022 年 8 月 8 日 (月)</option>
+            <option value="20220809">2022 年 8 月 9 日 (火)</option>
+            <option value="20220810">2022 年 8 月 10 日 (水)</option>
+            <option value="20220811">2022 年 8 月 11 日 (木)</option>
+            <option value="20220812">2022 年 8 月 12 日 (金)</option>
+            <option value="20220813">2022 年 8 月 13 日 (土)</option>
+            <option value="20220814">2022 年 8 月 14 日 (日)</option>
+            <option value="20220815">2022 年 8 月 15 日 (月)</option>
+            <option value="20220816">2022 年 8 月 16 日 (火)</option>
+            <option value="20220817">2022 年 8 月 17 日 (水)</option>
+            <option value="20220818">2022 年 8 月 18 日 (木)</option>
+            <option value="20220819">2022 年 8 月 19 日 (金)</option>
+            <option value="20220820">2022 年 8 月 20 日 (土)</option>
+            <option value="20220821">2022 年 8 月 21 日 (日)</option>
+        </select>
+        <input type="submit" name="submit" value="決定"/>
+    </form>
 
 </body>
 

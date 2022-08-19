e30b7bfb4763d3a85f0f220121683db607020836
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-20 00:14:46 +0900

    Update archive.php

--- bnaaltermuseum/archive.php
+++ bnaaltermuseum/archive.php
@@ -59,6 +59,7 @@ while ($row = fgetcsv($fp)) {
             padding: 0;
             margin: 0;
             width: 100%;
+            height: 100%;
             z-index: 0;
             overflow-y: auto;
             overflow-x: hidden;

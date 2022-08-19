0818fa1c889f2fb7282bbe6a0fb546606f299ca1
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-20 03:11:36 +0900

    Update archive.php

--- bnaaltermuseum/archive.php
+++ bnaaltermuseum/archive.php
@@ -238,6 +238,7 @@ while ($row = fgetcsv($fp)) {
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="flash.js"></script>
 <script type="text/javascript">
     $(function() {
         $('.change').hide();

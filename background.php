5b25cf6f31d1a739462c17b1682d3c37c81475d7
∧° ┐ <we.are.pe.hu@gmail.com>
2022-09-29 17:05:29 +0900

    Update background.php

--- background.php
+++ background.php
@@ -4,7 +4,11 @@ $today = date("Ymd");
 $source_file =  $today . ".csv";
 
 $fp = fopen($source_file, 'a+b');
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$symbol, $color, $timestamp, $ip,]);
+    rewind($fp);
+}
 flock($fp, LOCK_SH);
 while ($row = fgetcsv($fp)) {
     $rows[] = $row;

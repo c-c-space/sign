adac60ec5c3ce6ba2ca605cf510645523c88f7d2
∧° ┐ <we.are.pe.hu@gmail.com>
2022-09-30 02:46:17 +0900

    Update index.php

--- archive/index.php
+++ archive/index.php
@@ -16,7 +16,10 @@ $month = date("Ym");
 $source_file = "../" . $month . $day . ".csv";
 
 $fp = fopen($source_file, 'r');
-
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
 flock($fp, LOCK_UN);
 fclose($fp);
 

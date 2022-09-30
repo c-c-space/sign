f55ad7cfb2c703a85baad821f9415334d59e82dc
∧° ┐ <we.are.pe.hu@gmail.com>
2022-09-30 12:31:14 +0900

    Update script.js

--- submit/script.js
+++ submit/script.js
@@ -1,5 +1,5 @@
-var volume = new Tone.Volume(-40);
-var click = new Tone.PolySynth(4, Tone.Synth).chain(volume, Tone.Master);
+var volume = new Tone.Volume(-33);
+var click = new Tone.PolySynth(3, Tone.Synth).chain(volume, Tone.Master);
 var number = Tone.Frequency("B5").harmonize([
     7, 10, 12,
     10, 12, 14,

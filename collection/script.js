364c34a84856e361abd6596ffa0262ec81c9d7ce
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-04 00:19:22 +0900

    Update script.js

--- collection/script.js
+++ collection/script.js
@@ -13,7 +13,7 @@ let calendarHtml = '' // HTMLを組み立てる変数
 
 for (let d = 0; d < today; d++) {
     if (d < 9) {
-        calendarHtml += '<option value="0' + dayCount + '">0' + dayCount + month + year +'</option>'
+        calendarHtml += '<option value="0' + dayCount + '">0'  + year  + month + dayCount + '</option>'
         dayCount++
     }
     else if (dayCount > today) {

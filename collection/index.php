e59fa674f9ba4a42e96cac99938e78f5837d418d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-04 23:17:53 +0900

    sign

--- collection/index.php
+++ collection/index.php
@@ -187,7 +187,7 @@ fclose($fp);
             </u>
             <br />
             <span class="Etiquette">Colors</span>
-            <i class="Etiquette">and</i>
+            <i class="Etiquette">&</i>
             <span class="Etiquette">Symbols</span>
         </h1>
     </div>
--- index.php
+++ index.php
@@ -597,7 +597,7 @@ flock($fp, LOCK_UN);
                 <u>今日の気持ちを知る・表す</u>
                 <br />
                 <span class="Etiquette">Colors</span>
-                <i class="Etiquette">and</i>
+                <i class="Etiquette">&</i>
                 <span class="Etiquette">Symbols</span>
             </h1>
         </div>

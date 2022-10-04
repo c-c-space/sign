388d6a799b934afc8093ad259bee7182c1d976c3
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-04 20:34:06 +0900

    Update script.js

--- collection/script.js
+++ collection/script.js
@@ -1,7 +1,8 @@
-const date = new Date();
-const today = date.getDate();
-const year = date.getFullYear() ;
-const month = date.getmonth() + 1;
+const date = new Date()
+const today = date.getDate()
+const year = date.getFullYear()
+const month = date.getmonth()
+const thismonth = date.getmonth() +1
 
 const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
 const endDate = new Date(year, month, 0) // 月の最後の日を取得
@@ -13,13 +14,13 @@ let calendarHtml = '' // HTMLを組み立てる変数
 
 for (let d = 0; d < today; d++) {
     if (d < 9) {
-        calendarHtml += '<option value="0' + dayCount + '">' + year + '年' + month + '月' + dayCount + '日' + '</option>'
+        calendarHtml += '<option value="0' + dayCount + '">' + year + '年' + thismonth + '月' + dayCount + '日' + '</option>'
         dayCount++
     }
     else if (dayCount > today) {
     }
     else {
-        calendarHtml += '<option value="' + dayCount + '">' + year + '年' + month + '月' + dayCount + '日' + '</option>'
+        calendarHtml += '<option value="' + dayCount + '">' + year + '年' + thismonth + '月' + dayCount + '日' + '</option>'
         dayCount++
     }
 }

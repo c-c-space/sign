4c1887c1dc2e486e401c9b7e20b279d86113a91b
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-04 13:20:47 +0900

    Update index.php

--- collection/index.php
+++ collection/index.php
@@ -73,12 +73,9 @@ fclose($fp);
             </a>
             <span class="check"><b>✔</b></span>
         </div>
+            <a href="/sign/" target="_parent">SIGN</a>
         <div>
             <a class="tab" href="#flash">Flash</a>
-            <span class="check"><b>✔</b></span>
-        </div>
-        <div>
-            <a class="tab" href="/sign/" target="_parent">↩︎</a>
             <span class="check"><b>✔</b></span>
         </div>
     </div>

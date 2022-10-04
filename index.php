960781d9134083f411d6761cb5cdcea12e3facbc
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-04 19:45:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,6 @@ flock($fp, LOCK_UN);
         .Etiquette {
             font-family: "Etiquette";
             font-size: 200%;
-            font-weight: 900;
             filter: invert();
             padding-bottom: 0.25rem;
         }

65e61dd46e64a7efc05b569536c87ca1a182a02d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-10-04 20:37:15 +0900

    Update script.js

--- collection/script.js
+++ collection/script.js
@@ -8,18 +8,19 @@ const endDate = new Date(year, month, 0) // 月の最後の日を取得
 const endDayCount = endDate.getDate() // 月の末日
 
 let dayCount = 1 // 日にちのカウント
+let thismonth = month + 1 // 日にちのカウント
 let calendarHtml = '' // HTMLを組み立てる変数
 
 
 for (let d = 0; d < today; d++) {
     if (d < 9) {
-        calendarHtml += '<option value="0' + dayCount + '">' + year + '年' + month + '月' + dayCount + '日' + '</option>'
+        calendarHtml += '<option value="0' + dayCount + '">' + year + '年' + thismonth + '月' + dayCount + '日' + '</option>'
         dayCount++
     }
     else if (dayCount > today) {
     }
     else {
-        calendarHtml += '<option value="' + dayCount + '">' + year + '年' + month + '月' + dayCount + '日' + '</option>'
+        calendarHtml += '<option value="' + dayCount + '">' + year + '年' + thismonth + '月' + dayCount + '日' + '</option>'
         dayCount++
     }
 }

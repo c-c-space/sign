ebe1a68e94ace792368780260ff8e6e3fd5055e2
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-03 02:55:09 +0900

    cc

--- js/readyState.js
+++ js/readyState.js
@@ -1,5 +1,53 @@
 'use strict'
 
+async function submitThis() {
+  const symbolAll = document.getElementsByName('symbol');
+  const colorAll = document.getElementsByName("color");
+
+  let symbolValue = "";
+  for (let i = 0; i < symbolAll.length; i++) {
+    if (symbolAll[i].checked) {//(color1[i].checked === true)と同じ
+      symbolValue = symbolAll[i].value;
+      break;
+    }
+  }
+
+  let colorlValue = "";
+  for (let i = 0; i < colorAll.length; i++) {
+    if (colorAll[i].checked) {//(color1[i].checked === true)と同じ
+      colorlValue = colorAll[i].value;
+      break;
+    }
+  }
+
+  let thisSign = {
+    symbol : symbolValue,
+    color : colorlValue
+  };
+
+  const signJSON = JSON.stringify(thisSign);
+  let url = 'log.php';
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: signJSON
+  })
+
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+
+  setTimeout(() => {
+    location.reload();
+  }, 1000);
+}
+
 function changeHidden() {
   const mainAll = document.querySelectorAll('main');
   mainAll.forEach(main => {

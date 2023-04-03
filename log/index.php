6af6409eceae1cacaa81c483995148309f353d6e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-03 20:52:23 +0900

    Update index.php

--- log/index.php
+++ log/index.php
@@ -3,6 +3,10 @@ mb_language("ja");
 mb_internal_encoding("UTF-8");
 date_default_timezone_set('Asia/Tokyo');
 
+$site = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}";
+$url = "{$site}" . "{$_SERVER['REQUEST_URI']}";
+
+
 $day = date("d");
 if (isset($_GET["day"])) {
   $day = $_GET["day"];
@@ -37,6 +41,18 @@ fclose($fp);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no" />
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo $title . $description; ?>">
+  <meta property="og:title" content="<?php echo $title; ?>" />
+  <meta property="og:description" content="<?php echo $title . $description; ?>" />
+  <meta property="og:site_name" content="<?php echo $_SERVER['HTTP_HOST']; ?>" />
+  <meta property="og:url" content="<?php echo $url; ?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="ja_JP" />
+  <meta property="og:image" content="<?php echo $url; ?>card.png" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:image" content="<?php echo $url; ?>card.png" />
+
   <link rel="stylesheet" href="../style.css" />
   <link rel="stylesheet" href="../css/all.css" />
   <link rel="stylesheet" href="../css/flash.css" />

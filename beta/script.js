c14f0650ac9f3eb0ec2aa6e88476e59443289860
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-12 18:36:47 +0900

    cc

--- beta/script.js
+++ beta/script.js
@@ -23,42 +23,42 @@ selectModal.addEventListener('change', function() {
 });
 
 
+let all = document.querySelector('#all');
+let flash = document.querySelector('#flash');
+let now = document.querySelector('#now');
+
 function allView() {
-  let all = document.querySelector('#all');
-  let flash = document.querySelector('#flash');
   if (all.style.opacity == 1) {
     all.style.opacity = 0;
     flash.style.opacity = 0;
-    selectModal.style.opacity = 1;
+    now.style.opacity = 1;
     all.style.zIndex = 0;
     flash.style.zIndex = 0;
-    selectModal.style.zIndex = 1;
+    now.style.zIndex = 1;
   } else {
     all.style.opacity = 1;
     flash.style.opacity = 0;
-    selectModal.style.opacity = 0;
+    now.style.opacity = 0;
     all.style.zIndex = 1;
     flash.style.zIndex = 0;
-    selectModal.style.zIndex = 0;
+    now.style.zIndex = 0;
   }
 }
 
 function flashView() {
-  let flash = document.querySelector('#flash');
-  let all = document.querySelector('#all');
   if (flash.style.opacity == 1) {
     flash.style.opacity = 0;
     all.style.opacity = 0;
-    selectModal.style.opacity = 1;
+    now.style.opacity = 1;
     flash.style.zIndex = 0;
     all.style.zIndex = 0;
-    selectModal.style.zIndex = 1;
+    now.style.zIndex = 1;
   } else {
     flash.style.opacity = 1;
     all.style.opacity = 0;
-    selectModal.style.opacity = 0;
+    now.style.opacity = 0;
     flash.style.zIndex = 1;
     all.style.zIndex = 0;
-    selectModal.style.zIndex = 0;
+    now.style.zIndex = 0;
   }
 }

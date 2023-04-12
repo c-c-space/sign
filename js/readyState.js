88101f972bc9e3221aef9077a1e5798f36ac36b4
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-12 13:53:39 +0900

    cc

--- js/readyState.js
+++ js/readyState.js
@@ -6,9 +6,13 @@ function allView() {
   if (all.style.opacity == 1) {
     all.style.opacity = 0;
     flash.style.opacity = 0;
+    all.style.zIndex = 0;
+    flash.style.zIndex = 0;
   } else {
     all.style.opacity = 1;
     flash.style.opacity = 0;
+    all.style.zIndex = 1;
+    flash.style.zIndex = 0;
   }
 }
 
@@ -18,9 +22,13 @@ function flashView() {
   if (flash.style.opacity == 1) {
     flash.style.opacity = 0;
     all.style.opacity = 0;
+    flash.style.zIndex = 0;
+    all.style.zIndex = 0;
   } else {
     flash.style.opacity = 1;
     all.style.opacity = 0;
+    flash.style.zIndex = 1;
+    all.style.zIndex = 0;
   }
 }
 

f77d98735024017a57a362e60c44cca3e62219ad
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-12 21:45:08 +0900

    Update flash.js

--- js/flash.js
+++ js/flash.js
@@ -5,30 +5,18 @@ window.addEventListener('load', function () {
 });
 
 function viewSlide(className, i = -1) {
-  let liArray = document.querySelectorAll(className);
+  let liArr = document.querySelectorAll(className);
   if (i >= 0) {
-    liArray[i].style.opacity = 0;
+    liArr[i].style.opacity = 0;
   }
   i++;
-  if (i >= liArray.length) {
+  if (i >= liArr.length) {
     i = 0;
   }
-  liArray[i].style.opacity = 1;
+  liArr[i].style.opacity = 1;
   const speed = document.querySelector('#flash_speed')
   let msec = speed.max - speed.value;
   setTimeout(function () {
     viewSlide(className, i);
   }, msec);
 }
-
-function shuffleContent(container) {
-  let content = container.find("> *");
-  let total = content.length;
-  content.each(function() {
-    content.eq(Math.floor(Math.random() * total)).prependTo(container);
-  });
-}
-
-$(function() {
-  shuffleContent($("#flash ul"));
-});

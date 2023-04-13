a874853e3feb178ec6a4da63bef47c5d8c518782
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-13 15:27:50 +0900

    cc

---
+++ function.php
@@ -0,0 +1,25 @@
+<?php
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+date_default_timezone_set('Asia/Tokyo');
+
+$month = date("m");
+$day = date("d");
+$source_file = "log/". $month ."/". $day . ".csv";
+
+$forwardedFor = $_SERVER["REMOTE_ADDR"];
+
+$fp = fopen($source_file, 'a+b');
+
+$w = date("w");
+$week_name = array("日", "月", "火", "水", "木", "金", "土");
+
+function h($str)
+{
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+while ($row = fgetcsv($fp)) {
+  $rows[] = $row;
+}
+?>

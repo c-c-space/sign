1c0e70106ef27c458e4354c090b713bb59d2dca6
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-13 12:48:52 +0900

    Update index.php

--- beta/index.php
+++ beta/index.php
@@ -52,6 +52,14 @@ fclose($fp);
   <link rel="icon" href="icon.png" type="image/png">
   <link rel="apple-touch-icon-precomposed" href="icon.png" type="image/png">
   <style type="text/css">
+  html {
+    height: -webkit-fill-available;
+  }
+
+  body {
+    min-height: -webkit-fill-available;
+  }
+
   header {
     mix-blend-mode: difference;
   }
@@ -72,21 +80,6 @@ fclose($fp);
   #000,
   <?php endif; ?>
   #fff);">
-
-  <script src="/js/menu.js"></script>
-  <header id="menu" hidden>
-    <button id="js-button"><b></b></button>
-    <nav id="contents">
-      <a href="/" target="_parent">
-        <p><b>creative-community.space</b></p>
-        <u>Index</u>
-      </a>
-      <a href="/sign/" target="_parent">
-        <i>Colors and Symbols that Suits on Today</i>
-        <p>今日の気持ちを知る・表す</p>
-      </a>
-    </nav>
-  </header>
 
   <form id="now" class="hidden" method="GET">
     <button type="button" onclick="flashView()">

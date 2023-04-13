741dcdc24a7b48938ebf77e6fb3e958521d3a50a
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-13 15:11:45 +0900

    Update function.php

--- beta/function.php
+++ beta/function.php
@@ -6,22 +6,6 @@ $title = "令和三年三月";
 $year = "2021";
 $month = "03";
 
-//JSONデータを受け取り連想配列に変換
-$data = json_decode(file_get_contents('php://input'), true);
-
-$output = array(
-  "title" => $data["title"],
-  "year" => $data["year"],
-  "month" => $data["month"]
-);
-if (isset($output)) {
-  $title = $data["title"];
-  $year = $data["year"];
-  $month = $data["month"];
-}
-
-echo json_encode($output);
-
 $source_file = $year . $month . $day . ".csv";
 $fp = fopen($source_file, "a+b");
 $post = sizeof(file($source_file));

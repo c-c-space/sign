1482ccd6306fb6b682ee375a4b61c753c6af83b9
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-13 14:00:20 +0900

    Update function.php

--- beta/function.php
+++ beta/function.php
@@ -2,25 +2,27 @@
 mb_language("ja");
 mb_internal_encoding("UTF-8");
 
+$title = "令和三年三月";
 $year = "2021";
 $month = "03";
-$source_file = $year.$month.$day.".csv";
-$fp = fopen($source_file,'a+b');
+
+$source_file = $year . $month . $day . ".csv";
+$fp = fopen($source_file, "a+b");
 $post = sizeof(file($source_file));
 
-$title = '令和三年三月';
 $description = $post. 'の色と記号';
 
-$site = 'http'.(isset($_SERVER['HTTPS'])?'s':'').'://'."{$_SERVER['HTTP_HOST']}";
+$site = "http".(isset($_SERVER["HTTPS"])?"s":"")."://"."{$_SERVER["HTTP_HOST"]}";
 $url = "{$site}"."{$_SERVER['REQUEST_URI']}";
 
 function h($str)
 {
-  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
 }
 
 while ($row = fgetcsv($fp)) {
   $rows[] = $row;
 }
+
 fclose($fp);
 ?>

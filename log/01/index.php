965e6ea0299ac6191faaed47f215153edf2d47bb
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 17:19:55 +0900

    sign

--- log/01/index.php
+++ log/01/index.php
@@ -1,44 +1,23 @@
 <?php
+$title = "一月"
 $month = "01";
 $day = "01";
 
 require('../function.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="format-detection" content="telephone=no" />
+<script type="text/javascript">
+let thismonth = 1
+const endDate = 31
+</script>
 
-  <script type="text/javascript">
-  let thismonth = 1
-  const endDate = 31
-  </script>
+<main>
+  <?php require('../../php/viewall.php'); ?>
+</main>
+<script src="../../js/flash.js"></script>
+<script src="../../js/viewall.js"></script>
 
-  <link rel="stylesheet" href="../../style.css" />
-  <link rel="stylesheet" href="../../css/all.css" />
-  <link rel="stylesheet" href="../../css/flash.css" />
-</head>
-
-<body style="background-image: linear-gradient(0deg,
-  <?php if (!empty($rows)) : ?>
-  <?php foreach ($rows as $row) : ?>
-  #<?= h($row[1]) ?>,
-  <?php endforeach; ?>
-  <?php else : ?>
-  #000,
-  <?php endif; ?>
-  #fff);">
-
-  <main>
-    <?php require('../../php/viewall.php'); ?>
-  </main>
-  <script src="../../js/flash.js"></script>
-  <script src="../../js/viewall.js"></script>
-
-  <?php require('../now.php'); ?>
-  <script src="../selectdate.js" async></script>
+<?php require('../now.php'); ?>
+<script src="../selectdate.js" async></script>
 </body>
 </html>
--- log/function.php
+++ log/function.php
@@ -12,6 +12,11 @@ $fp = fopen($source_file, 'a+b');
 
 $post = sizeof(file($source_file));
 
+$description = $post .'の色と記号';
+
+$site = "http".(isset($_SERVER["HTTPS"])?"s":"")."://"."{$_SERVER["HTTP_HOST"]}";
+$url = "{$site}"."{$_SERVER['REQUEST_URI']}";
+
 function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
@@ -22,3 +27,36 @@ while ($row = fgetcsv($fp)) {
 
 fclose($fp);
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no" />
+  <title><?php echo $title ."の気持ちを知る・表す";?></title>
+  <meta name="description" content="<?php echo $title ."の気持ちを知る・表す". $description;?>">
+  <meta property="og:title" content="<?php echo $title ."の気持ちを知る・表す"; ?>">
+  <meta property="og:description" content="<?php echo $title ."の気持ちを知る・表す". $description;?>">
+  <meta property="og:site_name" content="<?php echo $site;?>">
+  <meta property="og:url" content="<?php echo $url;?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="ja_JP" />
+  <meta property="og:image" content="summary.png" />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:image" content="summary.png" />
+
+  <link rel="stylesheet" href="../../style.css" />
+  <link rel="stylesheet" href="../../css/all.css" />
+  <link rel="stylesheet" href="../../css/flash.css" />
+</head>
+
+<body style="background-image: linear-gradient(0deg,
+  <?php if (!empty($rows)) : ?>
+  <?php foreach ($rows as $row) : ?>
+  #<?= h($row[1]) ?>,
+  <?php endforeach; ?>
+  <?php else : ?>
+  #000,
+  <?php endif; ?>
+  #fff);">

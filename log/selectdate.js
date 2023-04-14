36d113a71286112d678a9c582553f18b93acf506
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 17:37:58 +0900

    Update selectdate.js

--- log/selectdate.js
+++ log/selectdate.js
@@ -3,13 +3,13 @@ let calendarHtml = '<option selected disabled>Select Date</option>'
 
 for (let d = 0; d < endDate; d++) {
   if (d < 9) {
-    calendarHtml += '<option value="0' + dayCount + '">' + thismonth + '月' + dayCount + '日' + '</option>'
+    calendarHtml += '<option value="0' + dayCount + '">' + thismonth + ' 月 0' + dayCount + ' 日' + '</option>'
     dayCount++
   }
   else if (dayCount > endDate) {
   }
   else {
-    calendarHtml += '<option value="' + dayCount + '">' + thismonth + '月' + dayCount + '日' + '</option>'
+    calendarHtml += '<option value="' + dayCount + '">' + thismonth + '月 ' + dayCount + ' 日' + '</option>'
     dayCount++
   }
 }

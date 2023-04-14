0aa1caba5fbca807329a0a728dfe8ba1b3ac2827
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 18:28:42 +0900

    sign

---
+++ collection/04/index.php
@@ -0,0 +1,22 @@
+<?php
+$month = "04";
+$day = "01";
+
+require('../function.php');
+?>
+
+<script type="text/javascript">
+let thismonth = 1
+const endDate = 30
+</script>
+
+<main>
+  <?php require('../../php/viewall.php'); ?>
+</main>
+<script src="../../js/flash.js"></script>
+<script src="../../js/viewall.js"></script>
+
+<?php require('../now.php'); ?>
+<script src="../selectdate.js" async></script>
+</body>
+</html>

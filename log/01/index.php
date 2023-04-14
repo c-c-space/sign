cdda3ad547df62bc87412ef9e1b5789585bc5bed
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 17:29:32 +0900

    Update index.php

--- log/01/index.php
+++ log/01/index.php
@@ -5,28 +5,18 @@ $day = "01";
 require('../function.php');
 ?>
 
-<body style="background-image: linear-gradient(0deg,
-  <?php if (!empty($rows)) : ?>
-  <?php foreach ($rows as $row) : ?>
-  #<?= h($row[1]) ?>,
-  <?php endforeach; ?>
-  <?php else : ?>
-  #000,
-  <?php endif; ?>
-  #fff);">
+<script type="text/javascript">
+let thismonth = 1
+const endDate = 31
+</script>
 
-  <script type="text/javascript">
-  let thismonth = 1
-  const endDate = 31
-  </script>
+<main>
+  <?php require('../../php/viewall.php'); ?>
+</main>
+<script src="../../js/flash.js"></script>
+<script src="../../js/viewall.js"></script>
 
-  <main>
-    <?php require('../../php/viewall.php'); ?>
-  </main>
-  <script src="../../js/flash.js"></script>
-  <script src="../../js/viewall.js"></script>
-
-  <?php require('../now.php'); ?>
-  <script src="../selectdate.js" async></script>
+<?php require('../now.php'); ?>
+<script src="../selectdate.js" async></script>
 </body>
 </html>

1ff319eaf7701861aada9e6bca9d7e199ef5eec2
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 12:58:29 +0900

    cc

--- beta/202103.php
+++ beta/202103.php
@@ -131,23 +131,18 @@ fclose($fp);
   <script type="text/javascript">
   let all = document.querySelector('#all');
   let flash = document.querySelector('#flash');
-  let log = document.querySelector('#log');
 
   function allView() {
     if (all.style.opacity == 1) {
       all.style.opacity = 0;
       flash.style.opacity = 0;
-      log.style.opacity = 1;
       all.style.zIndex = 0;
       flash.style.zIndex = 0;
-      log.style.zIndex = 100;
     } else {
       all.style.opacity = 1;
       flash.style.opacity = 0;
-      log.style.opacity = 0;
       all.style.zIndex = 1;
       flash.style.zIndex = 0;
-      log.style.zIndex = 0;
     }
   }
 
@@ -155,17 +150,13 @@ fclose($fp);
     if (flash.style.opacity == 1) {
       flash.style.opacity = 0;
       all.style.opacity = 0;
-      log.style.opacity = 1;
       flash.style.zIndex = 0;
       all.style.zIndex = 0;
-      log.style.zIndex = 100;
     } else {
       flash.style.opacity = 1;
       all.style.opacity = 0;
-      log.style.opacity = 0;
       flash.style.zIndex = 1;
       all.style.zIndex = 0;
-      log.style.zIndex = 0;
     }
   }
   </script>
--- beta/202106.php
+++ beta/202106.php
@@ -123,23 +123,18 @@ fclose($fp);
   <script type="text/javascript">
   let all = document.querySelector('#all');
   let flash = document.querySelector('#flash');
-  let log = document.querySelector('#log');
 
   function allView() {
     if (all.style.opacity == 1) {
       all.style.opacity = 0;
       flash.style.opacity = 0;
-      log.style.opacity = 1;
       all.style.zIndex = 0;
       flash.style.zIndex = 0;
-      log.style.zIndex = 100;
     } else {
       all.style.opacity = 1;
       flash.style.opacity = 0;
-      log.style.opacity = 0;
       all.style.zIndex = 1;
       flash.style.zIndex = 0;
-      log.style.zIndex = 0;
     }
   }
 
@@ -147,17 +142,13 @@ fclose($fp);
     if (flash.style.opacity == 1) {
       flash.style.opacity = 0;
       all.style.opacity = 0;
-      log.style.opacity = 1;
       flash.style.zIndex = 0;
       all.style.zIndex = 0;
-      log.style.zIndex = 100;
     } else {
       flash.style.opacity = 1;
       all.style.opacity = 0;
-      log.style.opacity = 0;
       flash.style.zIndex = 1;
       all.style.zIndex = 0;
-      log.style.zIndex = 0;
     }
   }
   </script>

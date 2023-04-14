3819bfe912ad5fbb8ef72cd22af4b4ddf9ec92f3
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 12:56:40 +0900

    cc

--- beta/202108.php
+++ beta/202108.php
@@ -120,6 +120,46 @@ fclose($fp);
     </section>
   </main>
 
-  <script src="script.js"></script>
+  <script type="text/javascript">
+  let all = document.querySelector('#all');
+  let flash = document.querySelector('#flash');
+  let log = document.querySelector('#log');
+
+  function allView() {
+    if (all.style.opacity == 1) {
+      all.style.opacity = 0;
+      flash.style.opacity = 0;
+      log.style.opacity = 1;
+      all.style.zIndex = 0;
+      flash.style.zIndex = 0;
+      log.style.zIndex = 100;
+    } else {
+      all.style.opacity = 1;
+      flash.style.opacity = 0;
+      log.style.opacity = 0;
+      all.style.zIndex = 1;
+      flash.style.zIndex = 0;
+      log.style.zIndex = 0;
+    }
+  }
+
+  function flashView() {
+    if (flash.style.opacity == 1) {
+      flash.style.opacity = 0;
+      all.style.opacity = 0;
+      log.style.opacity = 1;
+      flash.style.zIndex = 0;
+      all.style.zIndex = 0;
+      log.style.zIndex = 100;
+    } else {
+      flash.style.opacity = 1;
+      all.style.opacity = 0;
+      log.style.opacity = 0;
+      flash.style.zIndex = 1;
+      all.style.zIndex = 0;
+      log.style.zIndex = 0;
+    }
+  }
+  </script>
 </body>
 </html>

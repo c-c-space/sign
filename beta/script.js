acf090d9d359f3259299925df76986bd6465aaa6
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 13:31:14 +0900

    Update script.js

--- beta/script.js
+++ beta/script.js
@@ -1,45 +1,5 @@
 'use strict'
 
-let all = document.querySelector('#all');
-let flash = document.querySelector('#flash');
-let log = document.querySelector('#log');
-
-function allView() {
-  if (all.style.opacity == 1) {
-    all.style.opacity = 0;
-    flash.style.opacity = 0;
-    log.style.opacity = 1;
-    all.style.zIndex = 0;
-    flash.style.zIndex = 0;
-    log.style.zIndex = 100;
-  } else {
-    all.style.opacity = 1;
-    flash.style.opacity = 0;
-    log.style.opacity = 0;
-    all.style.zIndex = 1;
-    flash.style.zIndex = 0;
-    log.style.zIndex = 0;
-  }
-}
-
-function flashView() {
-  if (flash.style.opacity == 1) {
-    flash.style.opacity = 0;
-    all.style.opacity = 0;
-    log.style.opacity = 1;
-    flash.style.zIndex = 0;
-    all.style.zIndex = 0;
-    log.style.zIndex = 100;
-  } else {
-    flash.style.opacity = 1;
-    all.style.opacity = 0;
-    log.style.opacity = 0;
-    flash.style.zIndex = 1;
-    all.style.zIndex = 0;
-    log.style.zIndex = 0;
-  }
-}
-
 document.addEventListener('readystatechange', event => {
   if (event.target.readyState === 'loading') {
     // 文書の読み込み中に実行する

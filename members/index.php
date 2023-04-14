c120137cbcaef25e1c3307bbd3ff7bf6ce242ce6
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-14 18:59:57 +0900

    Update index.php

--- members/index.php
+++ members/index.php
@@ -88,43 +88,10 @@ fclose($fp);
   </form>
 
   <main>
-    <section id="all">
-      <ul>
-        <?php if (!empty($rows)) : ?>
-          <?php foreach ($rows as $row) : ?>
-            <li style="background:#<?= h($row[1]) ?>;">
-              <span style="color:#<?= h($row[1]) ?>;"><?= h($row[0]) ?></span>
-            </li>
-          <?php endforeach; ?>
-        <?php else : ?>
-          <li style="background:#000;">
-            <span style="color:#fff;">?</span>
-          </li>
-        <?php endif; ?>
-      </ul>
-    </section>
-
-    <section id="flash">
-      <ul>
-        <?php if (!empty($rows)) : ?>
-          <?php foreach ($rows as $row) : ?>
-            <li style="background:#<?= h($row[1]) ?>;">
-              <b style="color:#<?= h($row[1]) ?>;"><?= h($row[0]) ?></b>
-            </li>
-          <?php endforeach; ?>
-        <?php else : ?>
-          <li style="background:#aaa;">
-            <b style="color:#aaa;">?</b>
-          </li>
-        <?php endif; ?>
-      </ul>
-      <section id="speed">
-        <input id="flash_speed" type="range" value="" min="500" max="5000">
-      </section>
-      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script src="../js/flash.js" async></script>
-    </section>
+    <?php require('../beta/viewall.php'); ?>
   </main>
+  <script src="../js/flash.js"></script>
+  <script src="../js/viewall.js"></script>
 
   <script src="script.js"></script>
 

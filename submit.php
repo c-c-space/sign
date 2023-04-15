291b96c4a6992b1bb7b65ae2fb7fbcfb3368d709
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-15 14:33:17 +0900

    Update submit.php

--- submit.php
+++ submit.php
@@ -7,14 +7,20 @@ $month = date("m");
 $day = date("d");
 $source_file = "collection/". $month ."/". $day . ".csv";
 
+$w = date("w");
+$week_name = array("日", "月", "火", "水", "木", "金", "土");
+$timestamp = date('Y年n月j日') . "($week_name[$w]) " .date("g:i:s A");
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
 define("LOGFILE", $source_file);
 $data = json_decode(file_get_contents("php://input"), true);
 
 $output = array(
   $data["symbol"],
   $data["color"],
-  $data["timestamp"],
-  $_SERVER["REMOTE_ADDR"]
+  $timestamp,
+  $ip
 );
 
 $result = implode(',', $output);

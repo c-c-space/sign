fb42a20aaf8d90ca3f6fe8ab3217c7ba1ba784c2
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-15 15:52:47 +0900

    Update you.js

--- js/you.js
+++ js/you.js
@@ -2,7 +2,6 @@
 
 const yourAll = document.querySelector('#all ul')
 const yourFlash = document.querySelector('#flash ul')
-const yourIP = document.querySelector('#yourIP')
 const yourPost = document.querySelector('#yourpost')
 
 function deleteAll() {
@@ -14,14 +13,19 @@ function deleteAll() {
 }
 
 let yourInfo = JSON.parse(localStorage.getItem('yourInfo'));
+const yourIP = document.querySelector('#yourIP')
 yourIP.innerText = "IP " + yourInfo.ip
 
 if(!localStorage.getItem('sign')) {
+  yourAll.innerHTML += `<li><p><u style="background:#000;"><span style="color:#000;">?</span></u><b style="color:#fff;">Nothing Here</b></p></li>`
+  yourFlash.innerHTML += `<li style="background:#aaa;"><b style="color:#aaa;">?</b></li>`
+  yourPost.innerText = 'Nothing Here'
+  document.body.style.backgroundImage = `linear-gradient(0deg, #aaa, #fff)`
+
   location.replace('index.php');
 } else {
   let youJSON = JSON.parse(localStorage.getItem('sign'));
   yourPost.innerText = youJSON.length + ' の色と記号'
-  yourAll.innerHTML += `<li><p><button type="button" onclick="deleteAll()">全消去 | Delete All</button></p></li>`
 
   for (let i = 0; i < youJSON.length; i++) {
     let time = youJSON[i].timestamp
@@ -32,5 +36,6 @@ if(!localStorage.getItem('sign')) {
     yourFlash.innerHTML += `<li style="background:#${color};"><b style="color:#${color};">${symbol}</b></li>`
   }
 
+  yourAll.innerHTML += `<li><p><button type="button" onclick="deleteAll()">全消去 | Delete All</button></p></li>`
   document.body.style.backgroundImage = `linear-gradient(0deg, ${yourGradient} #fff)`
 }

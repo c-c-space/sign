1c5cb4f3d6d2fd663c9d2823d993e1c21a6e8830
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-01 22:32:43 +0900

    Update about.php

--- about.php
+++ about.php
@@ -5,18 +5,19 @@ $os = $_SERVER["HTTP_USER_AGENT"];
 ?>
 
 <section id="about">
-  <?php
-  echo "IP <b id='ip'>" . $ip . "</b> | ";
-  echo "PORT <b id='hqdn'>" . $hqdn . "</b><br/>";
-  echo "<small id='os'>" . $os . "</small>";
-  ?>
-  <br/>
   <button type="button" id="back-btn" onclick="setLOG()">自分の気持ちを知る・表す</button>
 
   <h1>This is an Online Communication Tool that Using Colors and Symbols that Suit on You.</h1>
   <p>これは、<b>色</b> と <b>記号</b> を使って <b>自分の気持ちを知る・表す</b> オンライン・コミュニケーションツール です。</p>
   <p> <b>_</b></p>
   <hr/>
+  <p>
+    <?php
+    echo "IP <b id='ip'>" . $ip . "</b> | ";
+    echo "PORT <b id='hqdn'>" . $hqdn . "</b><br/>";
+    echo "<small id='os'>" . $os . "</small>";
+    ?>
+  </p>
 </section>
 <!--
 <p><b>色</b> と <b>記号</b> の 投稿 を 日毎 に 記録し、<b>毎日</b> の気持ちを集めた作品を制作します。</p>

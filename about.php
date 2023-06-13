8e401ed2973f66fb82e9551527ef24dbe259f4ad
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-06-13 20:05:59 +0900

    Update about.php

--- about.php
+++ about.php
@@ -17,6 +17,7 @@
     <b>自分の気持ちを知る・表す</b>
     オンライン・コミュニケーションツール です。
   </p>
+  <br/>
   <h2>
     Create a Collection of Daily Colors and Symbols with Daily Posts.
   </h2>
@@ -28,6 +29,7 @@
     <b>気持ちを知る・表す</b>
     色 と 記号 ページを生成します。
   </p>
+  <br/>
   <h2>
     Posted Colors and Symbols are also Saved in Local Storage to Create Your Colors and Symbols.
   </h2>

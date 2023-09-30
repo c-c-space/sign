2ba9e1999525c5eda0b4cf5474dd568a76a659e1
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-30 13:01:45 +0900

    sign

--- collection/head.php
+++ collection/head.php
@@ -31,8 +31,8 @@ fclose($fp);
 
   <!--og:meta-->
   <meta content="website" property="og:type">
-  <title><?php echo $month;?> 月 <?php echo $day;?> 日 の気持ちを知る・表す</title>
-  <meta content="<?php echo $month;?> 月 <?php echo $day;?> 日 の気持ちを知る・表す" property="og:title">
+  <title><?php echo $month; ?> 月 <?php echo $day; ?> 日 の気持ちを知る・表す</title>
+  <meta content="<?php echo $month; ?> 月 <?php echo $day; ?> 日 の気持ちを知る・表す" property="og:title">
   <meta content="18 の色 と 35 の記号 を使って 自分の気持ちを知る・表す" name="description">
   <meta content="18 の色 と 35 の記号 を使って 自分の気持ちを知る・表す" name="og:description">
 
@@ -41,6 +41,18 @@ fclose($fp);
   <meta content="<?php echo $url; ?>" property="og:url">
   <meta content="<?php echo $site . "/sign/summary.png"; ?>" property="og:image">
   <meta content="<?php echo $site . "/sign/summary.png"; ?>" name="twitter:image:src">
+
+  <link rel="icon" href="/sign/img/favicon.png">
+  <link rel="icon" href="/sign/img/android.png" sizes="192x192">
+  <link rel="apple-touch-icon-precomposed" href="/sign/img/ios.png" sizes="180x180">
+
+  <meta name="application-name" content="creative-community.space" />
+  <meta name="msapplication-TileImage" content="/sign/img/favicon.png" />
+  <meta name="msapplication-square70x70logo" content="/sign/img/favicon.png" />
+  <meta name="msapplication-square150x150logo" content="/sign/img/ios.png" />
+  <meta name="msapplication-wide310x150logo" content="/sign/img/cover.png" />
+  <meta name="msapplication-square310x310logo" content="/sign/img/twitter.png" />
+  <meta name="msapplication-TileColor" content="#fff" />
 
   <link rel="stylesheet" href="/css/menu.css" />
   <link rel="stylesheet" href="/sign/css/style.css" />

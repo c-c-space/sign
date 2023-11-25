1b342e389fa69b35b1c5bec97d7235f349c0f64b
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-11-26 04:13:02 +0900

    sign

--- js/flash.js
+++ js/flash.js
@@ -1,8 +1,16 @@
 'use strict'
 
-window.addEventListener('load', function () {
+window.addEventListener("load", () => {
   viewSlide('#flash ul li');
-});
+  const cover = document.body
+  let signCount = document.querySelectorAll('#flash ul li').length;
+  if (signCount == 1) {
+    cover.style.backgroundSize = `100% ${signCount * 100}%`;
+  } else {
+    cover.style.backgroundSize = `100% ${signCount * 50}%`;
+  }
+  cover.style.animation = `gradient ${signCount * 5}s ease infinite`;
+}, false);
 
 function viewSlide(elem, i = -1) {
   let liArr = document.querySelectorAll(elem);

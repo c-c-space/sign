fc76d4cf79974da13c3c3e130460b8e0f61818f5
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-01-30 22:27:26 +0900

    Update head.php

--- collection/head.php
+++ collection/head.php
@@ -37,7 +37,7 @@ fclose($fp);
   <meta property="og:type" content="website">
   <meta property="og:title" content="<?php echo $month; ?> 月 <?php echo $day; ?> 日 の気持ちを知る・表す">
   <meta property="og:description" content="これは、18 の色 と 35 の記号 を使って 自分の気持ちを知る・表す オンラインコミュニケーションツールです。">
-  <meta property="og:image" content="/sign/img/cover.png">
+  <meta property="og:image" content="https://creative-community.space/sign/img/cover.png">
   
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image">
@@ -45,18 +45,18 @@ fclose($fp);
   <meta property="twitter:url" content="<?php echo $url; ?>">
   <meta name="twitter:title" content="<?php echo $month; ?> 月 <?php echo $day; ?> 日 の気持ちを知る・表す">
   <meta name="twitter:description" content="これは、18 の色 と 35 の記号 を使って 自分の気持ちを知る・表す オンラインコミュニケーションツールです。">
-  <meta name="twitter:image" content="/sign/img/cover.png">
+  <meta name="twitter:image" content="https://creative-community.space/sign/img/cover.png">
 
-  <link rel="icon" href="/sign/img/favicon.png">
-  <link rel="icon" href="/sign/img/android.png" sizes="192x192">
-  <link rel="apple-touch-icon-precomposed" href="/sign/img/ios.png" sizes="180x180">
+  <link rel="icon" href="https://creative-community.space/sign/img/favicon.png">
+  <link rel="icon" href="https://creative-community.space/sign/img/android.png" sizes="192x192">
+  <link rel="apple-touch-icon-precomposed" href="https://creative-community.space/sign/img/ios.png" sizes="180x180">
 
   <meta name="application-name" content="creative-community.space" />
-  <meta name="msapplication-TileImage" content="/sign/img/favicon.png" />
-  <meta name="msapplication-square70x70logo" content="/sign/img/favicon.png" />
-  <meta name="msapplication-square150x150logo" content="/sign/img/ios.png" />
-  <meta name="msapplication-wide310x150logo" content="/sign/img/cover.png" />
-  <meta name="msapplication-square310x310logo" content="/sign/img/twitter.png" />
+  <meta name="msapplication-TileImage" content="https://creative-community.space/sign/img/favicon.png" />
+  <meta name="msapplication-square70x70logo" content="https://creative-community.space/sign/img/favicon.png" />
+  <meta name="msapplication-square150x150logo" content="https://creative-community.space/sign/img/ios.png" />
+  <meta name="msapplication-wide310x150logo" content="https://creative-community.space/sign/img/cover.png" />
+  <meta name="msapplication-square310x310logo" content="https://creative-community.space/sign/img/twitter.png" />
   <meta name="msapplication-TileColor" content="#fff" />
 
   <link rel="stylesheet" href="/ver/css/menu.css" />
